728f0b0df4797f6e27cb61017745351238f0ff4f
biplabroy1 <biplabroy7318@gmail.com>
2023-04-02 14:20:45 +0530

    Global & Execution Context

--- advanceJS/tut1.js
+++ advanceJS/tut1.js
@@ -1,9 +1,9 @@
-console.log("Start to learn Advance Js");
+console.log("Start to learn Advance  Js.");
 //Global Execution:
-/*                JavaScript engine create a global execution context before it start to execute the code ans 
+/*                JavaScript engine create a global execution context before it start to execute the code and
                   A new execution context created everytime when function is called*/
 
 //Execution Stack :
-         /*       first all code store into the global execution and when any function call
-                  create one new execution context and store into the stack after that 
+         /*       first all code store into the global execution and when any function call then
+                  create a  new execution context and store into the stack after that 
                   the function run completly done then its remove from the stack. */
