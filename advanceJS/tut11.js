f75fd6ff236c14c1a7e9ad770c7163a94744d1a0
biplabroy1 <biplabroy7318@gmail.com>
2023-04-04 10:04:34 +0530

    Anonymous Function

---
+++ advanceJS/tut11.js
@@ -0,0 +1,13 @@
+//Anonymous Function : Its has no Function Name
+//Exam -1:
+let sum =function(a,b){
+    let add =a+b;
+    console.log(add);
+}
+sum(5,5);
+
+
+//Exam -2:
+document.addEventListener(function(){
+    console.log("Click Me");
+})
