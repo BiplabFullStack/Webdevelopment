610b53e4c358c08a44a42b6740f227bda676f665
biplabroy1 <biplabroy7318@gmail.com>
2023-04-04 20:33:24 +0530

    Immediately Invoke Function Expression(IIFE)

---
+++ advanceJS/tut13.js
@@ -0,0 +1,6 @@
+// Immediately Invoke Function Expression(IIFE):
+//Groping Function
+(function(name){
+    let surname ='Roy';
+    console.log(`I am a Backend Developer, My name is ${name} ${surname}`);
+})('Biplab');
