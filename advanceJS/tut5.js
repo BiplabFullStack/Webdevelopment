f7e7e1dceccea8c30071a5950368300cb2b2e7e6
biplabroy1 <biplabroy7318@gmail.com>
2023-04-04 09:52:56 +0530

    Use Strict

---
+++ advanceJS/tut5.js
@@ -0,0 +1,14 @@
+"use strict"
+function sum(a,a){  //This function is not work
+    var add=a+20;
+    console.log(add);
+}
+sum(10,12);
+
+/*
+"use strict"
+function sum(a){  //work This function 
+    var add=a+20;
+    console.log(add);
+}
+sum(10);              */
