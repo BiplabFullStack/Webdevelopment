c2558ea67acd4ae61f8169a154538406d4c7ffc0
biplabroy1 <biplabroy7318@gmail.com>
2023-04-18 09:10:36 +0530

    Local Storage Project -1

---
+++ webdevelopment/localStorageProject.js
@@ -0,0 +1,50 @@
+function onsignup(e) {
+    e.preventDefault();
+    const Expense = document.getElementById('Expense').value;
+    const Description = document.getElementById('Description').value;
+    const Category = document.getElementById('Category').value;
+
+    const obj = {
+        Expense,
+        Description,
+        Category
+    }
+    localStorage.setItem(obj.Expense, JSON.stringify(obj));
+    onScreenShow(obj);
+}
+window.addEventListener('DOMContentLoaded', () => {
+    Object.keys(localStorage).forEach((item) => {
+        onScreenShow(JSON.parse(localStorage[item]));
+    })
+})
+
+function onScreenShow(obj) {
+    const ul = document.getElementById('listOnScreen');
+    const li = document.createElement('li');
+    li.innerHTML = JSON.stringify(obj);
+
+    //Delite Button
+    const Delite = document.createElement('input');
+    Delite.type = 'button';
+    Delite.value = 'Delete';
+    Delite.onclick = () => {
+        localStorage.removeItem(obj.Expense);
+        ul.removeChild(li);
+    }
+
+    //Edit Button
+    const Edit = document.createElement('input');
+    Edit.type = 'button';
+    Edit.value = 'Edit';
+    Edit.onclick = () => {
+        localStorage.removeItem(obj.Expense);
+        ul.removeChild(li);
+        document.getElementById('Expense').value =obj.Expense;
+        document.getElementById('Description').value = obj.Description;
+        document.getElementById('Category').value = obj.ategory;
+    }
+    li.appendChild(Delite);
+    li.appendChild(Edit);
+    ul.appendChild(li);
+
+}
